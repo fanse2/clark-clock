d03b507ed58accc85d78f372755c0993cf995ac7
June Choi <fanse2@gmail.com>
2022-01-03 11:02:09 -0800

    add analog2

--- index.js
+++ index.js
@@ -71,22 +71,19 @@ class MainClock {
     
 }
 
-function showAnalog() {
+function showAnalog(td) {
     const secondHand = document.querySelector('.second-hand');
     const minsHand = document.querySelector('.min-hand');
     const hourHand = document.querySelector('.hour-hand');
 
-    const now = new Date();
-
-    const seconds = now.getSeconds();
+    let [hour, mins, seconds] = [td.hour(), td.minute(), td.second()];
+    
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
@@ -105,7 +102,7 @@ function startTime() {
     showTime(nClock,today);
     showTime(lClock,today);
     
-    showAnalog();
+    showAnalog(today);
     
     movPen(today.unix());
 
